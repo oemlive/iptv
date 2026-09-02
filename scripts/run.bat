64f274477ee1a7dd1d77b6e1f419960f880c0a49
oemlive <153254389+oemlive@users.noreply.github.com>
2026-09-02 17:10:58 +0800

    Delete scripts directory

--- scripts/run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd /d "%~dp0.."
-if not exist .venv (
-  python -m venv .venv
-)
-call .venv\Scripts\activate
-pip install -r requirements.txt
-uvicorn app.main:app --host 0.0.0.0 --port 8080
